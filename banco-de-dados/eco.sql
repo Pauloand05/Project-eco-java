7c35ab0b9adcd816ae141b9127092fccb5c2101d
Pauloand05 <pauloandre050619@gmail.com>
2025-06-06 01:16:02 -0300

    Atualizacao no banco

--- banco-de-dados/eco.sql
+++ banco-de-dados/eco.sql
@@ -1,192 +1,204 @@
--- MySQL Workbench Forward Engineering
+-- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
+--
+-- Host: localhost    Database: eco
+-- ------------------------------------------------------
+-- Server version	9.3.0
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema eco
--- -----------------------------------------------------
+--
+-- Table structure for table `atendimento`
+--
 
--- -----------------------------------------------------
--- Schema eco
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `eco` DEFAULT CHARACTER SET utf8mb3 ;
-USE `eco` ;
-
--- -----------------------------------------------------
--- Table `eco`.`funcionario`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `eco`.`funcionario` (
-  `codigo` VARCHAR(6) NOT NULL,
-  `nome` VARCHAR(100) NOT NULL,
-  `email` VARCHAR(100) NOT NULL,
-  `telefone` VARCHAR(45) NOT NULL,
-  `senha` VARCHAR(100) NOT NULL,
-  PRIMARY KEY (`codigo`),
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,
-  UNIQUE INDEX `telefone_UNIQUE` (`telefone` ASC) VISIBLE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `eco`.`endereco`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `eco`.`endereco` (
-  `cep` VARCHAR(8) NOT NULL,
-  `estado` VARCHAR(45) NOT NULL,
-  `cidade` VARCHAR(45) NOT NULL,
-  `bairro` VARCHAR(45) NOT NULL,
-  `logradouro` VARCHAR(100) NOT NULL,
-  PRIMARY KEY (`cep`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `eco`.`usuario`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `eco`.`usuario` (
-  `cpf` VARCHAR(11) NOT NULL,
-  `nome` VARCHAR(100) NOT NULL,
-  `nickname` VARCHAR(65) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  `telefone` VARCHAR(15) NULL DEFAULT NULL,
-  `senha` VARCHAR(100) NOT NULL,
-  PRIMARY KEY (`cpf`),
-  UNIQUE INDEX `usuariocol_UNIQUE` (`nickname` ASC) VISIBLE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `eco`.`denuncia`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `eco`.`denuncia` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `titulo` VARCHAR(45) NOT NULL,
-  `descricao` TEXT NOT NULL,
-  `data_criacao` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-  `data_atualizacao` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `usuario_cpf` VARCHAR(11) NOT NULL,
-  `endereco_cep` VARCHAR(8) NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_denuncia_usuario_idx` (`usuario_cpf` ASC) VISIBLE,
-  INDEX `fk_denuncia_endereco1_idx` (`endereco_cep` ASC) VISIBLE,
-  CONSTRAINT `fk_denuncia_endereco1`
-    FOREIGN KEY (`endereco_cep`)
-    REFERENCES `eco`.`endereco` (`cep`),
-  CONSTRAINT `fk_denuncia_usuario`
-    FOREIGN KEY (`usuario_cpf`)
-    REFERENCES `eco`.`usuario` (`cpf`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 101
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `eco`.`atendimento`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `eco`.`atendimento` (
-  `protocolo` BIGINT NOT NULL AUTO_INCREMENT,
-  `data_atendimento` DATE NOT NULL,
-  `status` ENUM('aberto', 'em atendimento', 'atendida') NOT NULL DEFAULT 'aberto',
-  `funcionario_codigo` VARCHAR(6) NOT NULL,
-  `denuncia_id` BIGINT NOT NULL,
+DROP TABLE IF EXISTS `atendimento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `atendimento` (
+  `protocolo` bigint NOT NULL AUTO_INCREMENT,
+  `data_atendimento` date NOT NULL,
+  `status` enum('aberto','em atendimento','atendida') NOT NULL DEFAULT 'aberto',
+  `funcionario_codigo` varchar(6) NOT NULL,
+  `denuncia_id` bigint NOT NULL,
   PRIMARY KEY (`protocolo`),
-  INDEX `fk_atendimento_admin1_idx` (`funcionario_codigo` ASC) VISIBLE,
-  INDEX `fk_atendimento_denuncia1_idx` (`denuncia_id` ASC) VISIBLE,
-  CONSTRAINT `fk_atendimento_admin1`
-    FOREIGN KEY (`funcionario_codigo`)
-    REFERENCES `eco`.`funcionario` (`codigo`),
-  CONSTRAINT `fk_atendimento_denuncia1`
-    FOREIGN KEY (`denuncia_id`)
-    REFERENCES `eco`.`denuncia` (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
+  KEY `fk_atendimento_admin1_idx` (`funcionario_codigo`),
+  KEY `fk_atendimento_denuncia1_idx` (`denuncia_id`),
+  CONSTRAINT `fk_atendimento_admin1` FOREIGN KEY (`funcionario_codigo`) REFERENCES `funcionario` (`codigo`),
+  CONSTRAINT `fk_atendimento_denuncia1` FOREIGN KEY (`denuncia_id`) REFERENCES `denuncia` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `avaliacoes`
+--
 
--- -----------------------------------------------------
--- Table `eco`.`jogos`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `eco`.`jogos` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(100) NOT NULL,
-  `genero` VARCHAR(45) NOT NULL,
-  `descricao` TEXT NULL DEFAULT NULL,
-  `data_lancamento` DATE NULL DEFAULT NULL,
-  `desenvolvedor` VARCHAR(100) NULL DEFAULT NULL,
-  `link_jogo` VARCHAR(255) NULL DEFAULT NULL,
+DROP TABLE IF EXISTS `avaliacoes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `avaliacoes` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `usuario_cpf` varchar(11) NOT NULL,
+  `jogos_id` bigint NOT NULL,
+  `avaliacao` varchar(255) NOT NULL,
+  `comentario` text,
+  `data_criacao` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `unique_nome` (`nome` ASC) VISIBLE)
-ENGINE = InnoDB
-AUTO_INCREMENT = 1744
-DEFAULT CHARACTER SET = utf8mb3;
+  KEY `fk_avaliacoes_usuario1_idx` (`usuario_cpf`),
+  KEY `fk_avaliacoes_jogos1_idx` (`jogos_id`),
+  CONSTRAINT `fk_avaliacoes_jogos1` FOREIGN KEY (`jogos_id`) REFERENCES `jogos` (`id`),
+  CONSTRAINT `fk_avaliacoes_usuario1` FOREIGN KEY (`usuario_cpf`) REFERENCES `usuario` (`cpf`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `denuncia`
+--
 
--- -----------------------------------------------------
--- Table `eco`.`avaliacoes`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `eco`.`avaliacoes` (
-  `id` BIGINT NOT NULL,
-  `usuario_cpf` VARCHAR(11) NOT NULL,
-  `jogos_id` BIGINT NOT NULL,
-  `avaliacao` ENUM('1', '2', '3', '4', '5') NOT NULL,
-  `comentario` TEXT NULL DEFAULT NULL,
-  `data_criacao` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+DROP TABLE IF EXISTS `denuncia`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `denuncia` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(45) NOT NULL,
+  `descricao` text NOT NULL,
+  `data_criacao` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `data_atualizacao` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `usuario_cpf` varchar(11) NOT NULL,
+  `endereco_cep` varchar(8) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_avaliacoes_usuario1_idx` (`usuario_cpf` ASC) VISIBLE,
-  INDEX `fk_avaliacoes_jogos1_idx` (`jogos_id` ASC) VISIBLE,
-  CONSTRAINT `fk_avaliacoes_jogos1`
-    FOREIGN KEY (`jogos_id`)
-    REFERENCES `eco`.`jogos` (`id`),
-  CONSTRAINT `fk_avaliacoes_usuario1`
-    FOREIGN KEY (`usuario_cpf`)
-    REFERENCES `eco`.`usuario` (`cpf`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 5
-DEFAULT CHARACTER SET = utf8mb3;
+  KEY `fk_denuncia_usuario_idx` (`usuario_cpf`),
+  KEY `fk_denuncia_endereco1_idx` (`endereco_cep`),
+  CONSTRAINT `fk_denuncia_endereco1` FOREIGN KEY (`endereco_cep`) REFERENCES `endereco` (`cep`),
+  CONSTRAINT `fk_denuncia_usuario` FOREIGN KEY (`usuario_cpf`) REFERENCES `usuario` (`cpf`)
+) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `endereco`
+--
 
--- -----------------------------------------------------
--- Table `eco`.`horarios_coleta`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `eco`.`horarios_coleta` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `dia_semana` ENUM('segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sabado', 'domingo') NULL DEFAULT NULL,
-  `turno` ENUM('matutino', 'vespertino', 'noturno') NULL DEFAULT NULL,
-  `endereco_cep` VARCHAR(8) NOT NULL,
+DROP TABLE IF EXISTS `endereco`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `endereco` (
+  `cep` varchar(8) NOT NULL,
+  `estado` varchar(45) NOT NULL,
+  `cidade` varchar(45) NOT NULL,
+  `bairro` varchar(45) NOT NULL,
+  `logradouro` varchar(100) NOT NULL,
+  PRIMARY KEY (`cep`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `funcionario`
+--
+
+DROP TABLE IF EXISTS `funcionario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `funcionario` (
+  `codigo` varchar(6) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `telefone` varchar(45) NOT NULL,
+  `senha` varchar(100) NOT NULL,
+  PRIMARY KEY (`codigo`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  UNIQUE KEY `telefone_UNIQUE` (`telefone`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `horarios_coleta`
+--
+
+DROP TABLE IF EXISTS `horarios_coleta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `horarios_coleta` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `dia_semana` enum('segunda','terça','quarta','quinta','sexta','sabado','domingo') DEFAULT NULL,
+  `turno` enum('matutino','vespertino','noturno') DEFAULT NULL,
+  `endereco_cep` varchar(8) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `endereco_cep` (`endereco_cep` ASC) VISIBLE,
-  CONSTRAINT `horarios_coleta_ibfk_1`
-    FOREIGN KEY (`endereco_cep`)
-    REFERENCES `eco`.`endereco` (`cep`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 21
-DEFAULT CHARACTER SET = utf8mb3;
+  KEY `endereco_cep` (`endereco_cep`),
+  CONSTRAINT `horarios_coleta_ibfk_1` FOREIGN KEY (`endereco_cep`) REFERENCES `endereco` (`cep`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `jogos`
+--
 
--- -----------------------------------------------------
--- Table `eco`.`resposta`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `eco`.`resposta` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `mensagem` TEXT NOT NULL,
-  `atendimento_protocolo` BIGINT NOT NULL,
-  `data_resposta` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+DROP TABLE IF EXISTS `jogos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `jogos` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `genero` varchar(45) NOT NULL,
+  `descricao` text,
+  `data_lancamento` date DEFAULT NULL,
+  `desenvolvedor` varchar(100) DEFAULT NULL,
+  `link_jogo` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_resposta_atendimento1_idx` (`atendimento_protocolo` ASC) VISIBLE,
-  CONSTRAINT `fk_resposta_atendimento1`
-    FOREIGN KEY (`atendimento_protocolo`)
-    REFERENCES `eco`.`atendimento` (`protocolo`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
+  UNIQUE KEY `unique_nome` (`nome`)
+) ENGINE=InnoDB AUTO_INCREMENT=1745 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `resposta`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+DROP TABLE IF EXISTS `resposta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `resposta` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `mensagem` text NOT NULL,
+  `atendimento_protocolo` bigint NOT NULL,
+  `data_resposta` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `fk_resposta_atendimento1_idx` (`atendimento_protocolo`),
+  CONSTRAINT `fk_resposta_atendimento1` FOREIGN KEY (`atendimento_protocolo`) REFERENCES `atendimento` (`protocolo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `cpf` varchar(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `nickname` varchar(65) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `telefone` varchar(15) DEFAULT NULL,
+  `senha` varchar(100) NOT NULL,
+  PRIMARY KEY (`cpf`),
+  UNIQUE KEY `usuariocol_UNIQUE` (`nickname`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-06-06  1:10:31
