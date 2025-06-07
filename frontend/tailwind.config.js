a4cffc9964c85a2a5495a59da9cf254f305a2516
Andros <joseedimilsonh@gmail.com>
2025-06-07 19:41:39 -0300

    front feito

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        border: 'hsl(var(--border))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+      },
+    },
+  },
+  plugins: [],
+};
